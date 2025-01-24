57e51d13f9a3ca5f71457d4d235788cab7cc03b8
CropCircle <circlecrop@outlook.com>
2025-01-24 10:00:48 +0800

    取消任务列表

--- Program.cs
+++ Program.cs
@@ -1,59 +1,86 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 
 class Program {
-    private const int ProgressBarMargin = 7;
-    static string GetProgressBar(int percentage) {
-        StringBuilder processBar = new StringBuilder();
-        int percentageCount = (int)((percentage / 100.0f) * (Console.WindowWidth - ProgressBarMargin));
-        int remainCount = Console.WindowWidth - percentageCount - ProgressBarMargin;
-        processBar.Append('\r');
-        processBar.Append(string.Concat(Enumerable.Repeat('=', percentageCount)));
-        processBar.Append('>');
-        processBar.Append(string.Concat(Enumerable.Repeat('-', remainCount)));
-        processBar.Append($"[{percentage,3}%]");
-        return processBar.ToString();
-    }
+    static readonly CancellationTokenSource s_cts = new CancellationTokenSource();
 
-    static async Task Main(string[] args) {
-        for (int i = 0; i <= 100; i++) {
-            Console.Write(GetProgressBar(i));
-            await Task.Delay(20);
-        }
-        Console.WriteLine("\nDone!");
-    }
+    static readonly HttpClient s_client = new HttpClient {
+        MaxResponseContentBufferSize = 1_000_000
+    };
 
-    public async Task ProcessMultipleWritesAsync() {
-        IList<FileStream> sourceStreams = new List<FileStream>();
+    static readonly IEnumerable<string> s_urlList = new string[]
+    {
+            "https://learn.microsoft.com",
+            "https://learn.microsoft.com/aspnet/core",
+            "https://learn.microsoft.com/azure",
+            "https://learn.microsoft.com/azure/devops",
+            "https://learn.microsoft.com/dotnet",
+            "https://learn.microsoft.com/dynamics365",
+            "https://learn.microsoft.com/education",
+            "https://learn.microsoft.com/enterprise-mobility-security",
+            "https://learn.microsoft.com/gaming",
+            "https://learn.microsoft.com/graph",
+            "https://learn.microsoft.com/microsoft-365",
+            "https://learn.microsoft.com/office",
+            "https://learn.microsoft.com/powershell",
+            "https://learn.microsoft.com/sql",
+            "https://learn.microsoft.com/surface",
+            "https://learn.microsoft.com/system-center",
+            "https://learn.microsoft.com/visualstudio",
+            "https://learn.microsoft.com/windows",
+            "https://learn.microsoft.com/maui"
+    };
 
-        try {
-            string folder = Directory.CreateDirectory("tempfolder").Name;
-            IList<Task> writeTaskList = new List<Task>();
+    static async Task Main() {
+        Console.WriteLine("Application started.");
+        Console.WriteLine("Press the ENTER key to cancel...\n");
 
-            for (int index = 1; index <= 10; ++index) {
-                string fileName = $"file-{index:00}.txt";
-                string filePath = $"{folder}/{fileName}";
-
-                string text = $"In file {index}{Environment.NewLine}";
-                byte[] encodedText = Encoding.Unicode.GetBytes(text);
-
-                var sourceStream =
-                    new FileStream(
-                        filePath,
-                        FileMode.Create, FileAccess.Write, FileShare.None,
-                        bufferSize: 4096, useAsync: true);
-
-                Task writeTask = sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
-                sourceStreams.Add(sourceStream);
-
-                writeTaskList.Add(writeTask);
+        Task cancelTask = Task.Run(() => {
+            while (Console.ReadKey().Key != ConsoleKey.Enter) {
+                Console.WriteLine("Press the ENTER key to cancel...");
             }
 
-            await Task.WhenAll(writeTaskList);
-        } finally {
-            foreach (FileStream sourceStream in sourceStreams) {
-                sourceStream.Close();
+            Console.WriteLine("\nENTER key pressed: cancelling downloads.\n");
+            s_cts.Cancel();
+        });
+
+        Task sumPageSizesTask = SumPageSizesAsync();
+
+        Task finishedTask = await Task.WhenAny(new[] { cancelTask, sumPageSizesTask });
+        if (finishedTask == cancelTask) {
+            // wait for the cancellation to take place:
+            try {
+                await sumPageSizesTask;
+                Console.WriteLine("Download task completed before cancel request was processed.");
+            } catch (OperationCanceledException) {
+                Console.WriteLine("Download task has been cancelled.");
             }
         }
+
+        Console.WriteLine("Application ending.");
+    }
+
+    static async Task SumPageSizesAsync() {
+        var stopwatch = Stopwatch.StartNew();
+
+        int total = 0;
+        foreach (string url in s_urlList) {
+            int contentLength = await ProcessUrlAsync(url, s_client, s_cts.Token);
+            total += contentLength;
+        }
+
+        stopwatch.Stop();
+
+        Console.WriteLine($"\nTotal bytes returned:  {total:#,#}");
+        Console.WriteLine($"Elapsed time:          {stopwatch.Elapsed}\n");
+    }
+
+    static async Task<int> ProcessUrlAsync(string url, HttpClient client, CancellationToken token) {
+        HttpResponseMessage response = await client.GetAsync(url, token);
+        byte[] content = await response.Content.ReadAsByteArrayAsync(token);
+        Console.WriteLine($"{url,-60} {content.Length,10:#,#}");
+
+        return content.Length;
     }
 }
 
